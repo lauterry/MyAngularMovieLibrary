0766637d7fdf0100b4b908371e7b7c4ef883800d
LAU Thierry <lau.t@sfeir.com>
2012-06-27 15:58:41 +0200

    Angular Bootstrap and dynamic view

---
+++ js/controllers.js
@@ -0,0 +1,16 @@
+function MovieListCtrl($scope) {
+    $scope.movies = [
+        {"title":"Avatar",
+            "year":2009,
+            "directors":"James Cameron",
+            "stars":"Sam Worthington, Zoe Saldana and Sigourney Weaver"},
+        {"name":"The Grudge",
+            "year":2004,
+            "directors":"Takashi Shimizu",
+            "stars":"Sarah Michelle Gellar, Jason Behr and Clea DuVall"},
+        {"name":"The Expandables",
+            "year":2010,
+            "directors":"Sylvester Stallone",
+            "stars":"Sylvester Stallone, Jason Statham and Jet Li"}
+    ];
+}
