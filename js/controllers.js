cb05a774ee7681e2caa5b0204f97b8d9b9287bd7
LAU Thierry <thierlau@gmail.com>
2012-06-29 18:16:06 +0300

    Fix type in model (replaced name with title)

--- js/controllers.js
+++ js/controllers.js
@@ -4,11 +4,11 @@ function MovieListCtrl($scope) {
             "year":2009,
             "directors":"James Cameron",
             "stars":"Sam Worthington, Zoe Saldana and Sigourney Weaver"},
-        {"name":"The Grudge",
+        {"title":"The Grudge",
             "year":2004,
             "directors":"Takashi Shimizu",
             "stars":"Sarah Michelle Gellar, Jason Behr and Clea DuVall"},
-        {"name":"The Expandables",
+        {"title":"The Expandables",
             "year":2010,
             "directors":"Sylvester Stallone",
             "stars":"Sylvester Stallone, Jason Statham and Jet Li"}
